7e115e30a8a5151ad4a2e4de142b87ab82af0443
Chris Smowton <smowton@github.com>
2023-06-06 14:58:11 +0100

    Improve latest JAR version selection
    This tries to account for the myriad ways people step outside the 'correct' format for Maven version numbers. We try to prefer the highest-numbered version while tolerating (but not preferring) arbitrary text suffixes, which can in practice be delimited by dots, dashes or nothing (e.g. "1.3beta4")
    
    We also explicitly depreciate versions using very large numbers on the theory that they are most likely datestamps which we shouldn't mistake for a very-recent version.

--- java/maven-indexer/pick-best-versions.py
+++ java/maven-indexer/pick-best-versions.py
@@ -1,33 +1,67 @@
+import re
 import sys
 
 all_major_versions = len(sys.argv) > 1 and sys.argv[1] == "--all-major-versions"
 
+digits = re.compile("^[0-9]+")
+
+# Returns a tuple consisting of the dot-separated integers forming a prefix of 'v', followed by the remainder as a string. For example, 1.2.3.4x5-6 will result in (1, 2, 3, 4, "x5-6")
+# Integer segments > 999 are assumed to be text-like rather than version numbers, and also trigger returning the rest of the version as a string.
 def version_to_tuple(v):
-    bits = v.split("-")
-    while len(bits) < 3:
-        bits.append("zzz") # No qualifiers --> sorts later than all versions with qualifiers
-    version_numbers = bits[0].split(".")
-    if len(version_numbers) == 2:
-        version_numbers.append("0")
-    if len(version_numbers) != 3:
-        return (v,)
-    try:
-        return tuple([int(n) for n in version_numbers] + bits[1:])
-    except ValueError:
-        return (v,)
+    result = []
+    while len(v) > 0:
+        nextnumber = digits.match(v)
+        if nextnumber is None:
+            break
+        nextnumber = nextnumber.group(0)
+        if len(nextnumber) > 3:
+            break
+        result.append(int(nextnumber))
+        skip = len(nextnumber)
+        if len(v) > skip and v[skip] == ".":
+            skip += 1
+        v = v[skip:]
+    if len(v) != 0:
+        result.append(v)
+    return result
+
+def element(v, n):
+    return v[n] if n < len(v) else None
+
+def type_rank(t):
+    # Rank integers before None before text (so e.g. 1.2.3.4 > 1.2.3 > 1.2.3-sometext)
+    if t is int:
+        return 2
+    elif t is type(None):
+        return 1
+    else:
+        return 0
+
+def type_cmp(t1, t2):
+    t1val = type_rank(t1)
+    t2val = type_rank(t2)
+    if t1val > t2val:
+        return -1
+    elif t1val < t2val:
+        return 1
+    else:
+        return 0
 
 def version_cmp(v1, v2):
     vt1 = version_to_tuple(v1)
     vt2 = version_to_tuple(v2)
-    if len(vt1) != len(vt2):
-        vt1 = (v1,)
-        vt2 = (v2,)
-    if vt2 > vt1:
-        return 1
-    elif vt2 < vt1:
-        return -1
-    else:
-        return 0
+
+    for i in range(max(len(vt1), len(vt2))):
+        e1 = element(vt1, i)
+        e2 = element(vt2, i)
+        if type(e1) != type(e2):
+            return type_cmp(type(e1), type(e2))
+        elif e1 > e2:
+            return -1
+        elif e1 < e2:
+            return 1
+
+    return 0
 
 byver = dict()
 
