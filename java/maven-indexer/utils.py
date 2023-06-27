b30aae3f1e44f5593cbc27926f50607fac3f7cdb
Chris Smowton <smowton@github.com>
2023-06-27 14:55:25 +0100

    Exclude Kotlin companion classes from class counts

--- java/maven-indexer/utils.py
+++ java/maven-indexer/utils.py
@@ -5,4 +5,6 @@ private_class_regex = re.compile(".*\\$[0-9]+\\.class$")
 def is_private(classfile):
   # Since we only have the index, not the class file headers, we have to underapproximate private classes.
   # Here the best we can currently do is recognise anonymous classes like MyClass$1.class.
-  return private_class_regex.match(classfile) is not None
+  # We also classify $Companion classes as private, since they are a Kotlin implementation detail and don't
+  # correspond to providing an additional user-facing name.
+  return private_class_regex.match(classfile) is not None or classfile.endswith("$Companion.class")
